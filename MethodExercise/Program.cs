eabf358173d0e07587df94c2f6a2fd911a29a3a3
AJeane <ajeane1924@gmail.com>
2021-06-14 22:43:54 -0500

    initial commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+
+            //string interpolation.
+
+            Console.WriteLine(); _ = $"Hello {name}. I heard about you, is your favorite animal still the {color} {animal}? Actually nevermind, though you said you like {band} right?";
+
+
+
+
+
+
+
+
+        }
+    }
+}
